c7233321a089b5c316dd29167cd2e0f5fe14917d
Paulo-galvao <paulogalvaoj21@gmail.com>
2024-07-01 21:00:03 -0300

    validação de número

--- index.js
+++ index.js
@@ -3,23 +3,28 @@ const prompt = require("prompt-sync")({ sigint: true });
 const algarismos = [];
 let numero = prompt("Digite um número: ");
 
-function maiorNumero(numero) {
+function ehNumero(num) {
+    return !isNaN(Number(num));
+}
 
-    numero.split('').forEach(alg => {
-        algarismos.push(parseFloat(alg));
-    });
+function maiorNumero(num) {
 
-    for(let i = 0; i < algarismos.length; i++) {
-        for(let j = i + 1; j < algarismos.length; j++) {
-            if(algarismos[i] < algarismos[j]) {
-                let aux = algarismos[j];
-                algarismos[j] = algarismos[i];
-                algarismos[i] = aux;
-            } 
+    if(ehNumero(num)) {
+        const algarismos = num.split("");
+        for(let i = 0; i < algarismos.length; i++) {
+            for(let j = i + 1; j < algarismos.length; j++) {
+                if(algarismos[i] < algarismos[j]) {
+                    let aux = algarismos[j];
+                    algarismos[j] = algarismos[i];
+                    algarismos[i] = aux;
+                } 
+            }
         }
+        return algarismos.join('');
     }
-    return algarismos.join('');
-    
+
+    console.log("O valor digitado não é um número!");
+    return false   
     
 }
 
